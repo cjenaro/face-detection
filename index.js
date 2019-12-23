946121cd9236487f3e6ca95a339f55a2637ab01e
Jenaro <jen.calvineo@gmail.com>
2019-12-23 10:34:56 -0300

    Face detection API

---
+++ index.js
@@ -0,0 +1,69 @@
+const webcam = document.querySelector('.webcam')
+const video = document.querySelector('.video')
+const face = document.querySelector('.face')
+const SIZE = 10
+const SCALE = 1.5
+const ctx = video.getContext('2d')
+const faceCtx = face.getContext('2d')
+
+if (!window.FaceDetector) {
+  document.body.innerHTML = `
+    <h1>Your browser/OS does not support Face Detector</h1>
+  `
+} else {
+  const faceDetector = new window.FaceDetector()
+
+  async function populateVideo() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { width: 1280, height: 720 },
+    })
+
+    webcam.srcObject = stream
+    await webcam.play()
+
+    video.width = webcam.videoWidth
+    video.height = webcam.videoHeight
+    face.width = webcam.videoWidth
+    face.height = webcam.videoHeight
+  }
+
+  async function detect() {
+    const faces = await faceDetector.detect(webcam)
+    faces.forEach(censor)
+    requestAnimationFrame(detect)
+  }
+
+  function censor({ boundingBox }) {
+    faceCtx.imageSmoothingEnabled = false
+    faceCtx.clearRect(0, 0, face.width, face.height)
+
+    const width = boundingBox.width * SCALE
+    const height = boundingBox.height * SCALE
+
+    faceCtx.drawImg(
+      webcam,
+      boundingBox.x,
+      boundingBox.y,
+      width,
+      height,
+      boundingBox.x,
+      boundingBox.y,
+      SIZE,
+      SIZE
+    )
+
+    faceCtx.drawImg(
+      face,
+      boundingBox.x,
+      boundingBox.y,
+      SIZE,
+      SIZE,
+      boundingBox.x - (width - boundingBox.width) / 2,
+      boundingBox.y - (height - boundingBox.height) / 2,
+      width,
+      height
+    )
+  }
+
+  populateVideo().then(detect)
+}
